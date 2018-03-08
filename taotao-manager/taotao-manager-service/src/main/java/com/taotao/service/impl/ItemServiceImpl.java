3ff0f06ce397e3a05a9de52ebcfba592a7002b20
yangge007 <yangshen9808@163.com>
2018-03-08 08:30:54 +0800

    '项目搭建成功并测试完毕';

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,35 @@
+package com.taotao.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import com.taotao.pojo.TbItemExample.Criteria;
+import com.taotao.service.ItemService;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	@Override
+	public TbItem getItemById(Long itemId) {
+//		itemMapper.selectByPrimaryKey(itemId);
+		TbItemExample example = new TbItemExample();
+		//创建查询条件
+		Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(itemId);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		TbItem item = null;
+		if(list != null && list.size() > 0) {
+			item = list.get(0);
+		}
+		return item;
+	}
+
+}
